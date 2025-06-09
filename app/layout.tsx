017cacd4dd4ea9f7820f03a2418fe1c03445aac4
Georgio Monsef <georgiomonsef@mac.home>
2025-06-09 12:54:21 -0400

    Add Meta Pixel code to <head> on all pages as per Meta instructions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import Script from "next/script"
 
 export const metadata = {
   title: "Blues Cleaning Crew | Professional Window Cleaning",
@@ -27,6 +28,32 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '24238468525754846');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Facebook Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '24238468525754846');
+        fbq('track', 'PageView');` }} />
+        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=24238468525754846&ev=PageView&noscript=1" /></noscript>
       </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
